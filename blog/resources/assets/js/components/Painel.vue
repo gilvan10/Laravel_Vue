f447a2adb8b4318a27321490e625e8fc40cf1ef4
gilvan <gilvananjos10ns@gmail.com>
2020-01-17 14:25:04 -0300

    primeiro commit

---
+++ blog/resources/assets/js/components/Painel.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div v-bind:class="defineCor">
+        <div class="panel-heading">{{titulo}}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props:['titulo','cor'],
+        computed:{
+            defineCor: function(){
+                return "panel "+ (this.cor || "panel-default");
+            }
+        }
+    }
+</script>
+
+<style media="screen">
+    .blue {
+         border-color: #0c73ad;
+     }
+    .blue > .panel-heading {
+        color: #f5f8fa;
+        background-color: #0c73ad;
+        border-color: #0c73ad;
+    }
+    .orange {
+        border-color: #ff8a3d;
+    }
+    .orange > .panel-heading {
+        color: #f5f8fa;
+        background-color: #ff8a3d;
+        border-color: #ff8a3d;
+    }
+</style>
---
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho="12">
+        <painel titulo="Lista de Artigos">
+            <tabela-lista
+                    v-bind:titulos="['#','Título','Descrição']"
+                    v-bind:itens="[[1,'PHP OO','Curso de PHP OO'],[2,'Vue JS','Curso de Vue JS']]"
+                    criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="7887522323"
+
+            ></tabela-lista>
+
+
+        </painel>
+    </pagina>
+@endsection
